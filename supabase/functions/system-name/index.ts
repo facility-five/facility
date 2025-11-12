a3c0331bdca8016b59eeb01760b70666a39285b5
valida_new <validanr1@gmail.com>
2025-11-12 02:28:18 -0300

    Allow CORS on invite/system functions

--- supabase/functions/system-name/index.ts
+++ supabase/functions/system-name/index.ts
@@ -3,7 +3,9 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2.45.0"
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Credentials": "true",
 }
 
 const SUPABASE_URL = Deno.env.get("SUPABASE_URL") ?? ""
